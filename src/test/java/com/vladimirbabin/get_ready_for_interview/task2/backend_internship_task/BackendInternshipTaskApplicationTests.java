44bde202a411828eccdab36be1319cb5aaa92c02
VladimirBabin <vladimir.sol.rojo@gmail.com>
2022-05-23 15:30:14 +0300

    some message

---
+++ src/test/java/com/vladimirbabin/get_ready_for_interview/task2/backend_internship_task/BackendInternshipTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirbabin.get_ready_for_interview.task2.backend_internship_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInternshipTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
